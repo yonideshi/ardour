737f6da378f7931c27acb285801acf8d74ec354e
Paul Davis <paul@linuxaudiosystems.com>
2015-07-09 12:54:09 -0400

    hide tabs in main window when there's only 1 tab left.
    This helps with space and also prevents tearing off the last tab

--- gtk2_ardour/ardour_ui.h
+++ gtk2_ardour/ardour_ui.h
@@ -823,6 +823,8 @@ class ARDOUR_UI : public Gtkmm2ext::UI, public ARDOUR::SessionHandlePtr
 	DuplicateRouteDialog* duplicate_routes_dialog;
 
 	void tabs_switch (GtkNotebookPage*, guint page_number);
+	void tabs_page_added (Gtk::Widget*, guint page_number);
+	void tabs_page_removed (Gtk::Widget*, guint page_number);
 	bool key_press_focus_accelerator_handler (Gtk::Window& window, GdkEventKey* ev, Gtkmm2ext::Bindings*);
 };
 
