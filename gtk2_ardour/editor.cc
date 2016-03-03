73835d90df5a9580230cd00cd970a23a23393306
Paul Davis <paul@linuxaudiosystems.com>
2016-03-03 12:02:35 -0500

    fix restoration of editor pane positions

--- gtk2_ardour/editor.cc
+++ gtk2_ardour/editor.cc
@@ -3774,7 +3774,7 @@ Editor::pane_allocation_handler (Allocation &alloc, Paned* which)
 	int pos;
 	XMLProperty* prop;
 	char buf[32];
-	XMLNode* node = ARDOUR_UI::instance()->editor_settings();
+	XMLNode* geometry = ARDOUR_UI::instance()->editor_settings();
 
 	enum Pane {
 		Horizontal = 0x1,
@@ -3782,8 +3782,6 @@ Editor::pane_allocation_handler (Allocation &alloc, Paned* which)
 	};
 
 	static Pane done;
-
-	XMLNode* geometry = find_named_node (*node, "geometry");
 
 	if (which == static_cast<Paned*> (&edit_pane)) {
 
@@ -3803,7 +3801,7 @@ Editor::pane_allocation_handler (Allocation &alloc, Paned* which)
 			pos = atoi (prop->value());
 		}
 
-		if (GTK_WIDGET(edit_pane.gobj())->allocation.width > pos) {
+		if (edit_pane.get_allocation().get_width() > pos) {
 			edit_pane.set_position (pos);
 		}
 
@@ -3824,7 +3822,7 @@ Editor::pane_allocation_handler (Allocation &alloc, Paned* which)
 			pos = atoi (prop->value());
 		}
 
-		if (GTK_WIDGET(editor_summary_pane.gobj())->allocation.height > pos) {
+		if (editor_summary_pane.get_allocation().get_height() > pos) {
 			editor_summary_pane.set_position (pos);
 		}
 
